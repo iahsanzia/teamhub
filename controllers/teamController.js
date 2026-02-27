c5502308d446c9cb3e6645a17985bdd71afcd31e
Ahsan Zia <mynameisahsanzia@gmail.com>
2026-02-27 15:51:31 +0500

    Team model created along with few controllers

---
+++ controllers/teamController.js
@@ -0,0 +1,58 @@
+const Team = require("./../models/Team");
+const User = require("./../models/User");
+const catchAsync = require("./../utils/catchAsync");
+const AppError = require("./../utils/appError");
+
+exports.createTeam = catchAsync(async (req, res, next) => {
+  const { name, description } = req.body;
+
+  const team = await Team.create({
+    name,
+    description,
+    owner: req.user.id,
+    members: [req.user.id],
+  });
+  res.status(201).json({
+    status: "success",
+    data: { team },
+  });
+});
+
+exports.getMyTeams = catchAsync(async (req, res, next) => {
+  const teams = await Team.find({
+    members: req.user.id,
+  }).populate("owner members", "name email");
+
+  res.status(200).json({
+    status: "success",
+    results: teams.length,
+    data: { teams },
+  });
+});
+
+exports.addMember = catchAsync(async (req, res, next) => {
+  const { teamId } = req.params;
+  const { userId } = req.body;
+
+  const team = await Team.findById(teamId);
+
+  if (!team) return next(new AppError("Team not found", 404));
+
+  if (team.owner.toString() !== req.user.id)
+    return next(new AppError("Only Owner can Add Members", 403));
+
+  if (!team.members.includes(userId)) {
+    team.members.push(userId);
+    await team.save();
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: { team },
+  });
+});
+
+exports.removeMember = catchAsync(async (req, res, next) => {
+  const { teamId, userId } = req.params;
+  const team = await Team.findById(teamId);
+});
